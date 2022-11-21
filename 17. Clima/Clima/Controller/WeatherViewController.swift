2baec55ebbc4421df2a6ec04501e9033353c1d92
Mutlu Çalkan <mutlu.calkan@gmail.com>
2022-11-21 11:00:28 +0300

    Clima app project created

---
+++ 17. Clima/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Mutlu Çalkan on 18/11/2022.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchButtonDidPress(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        }else {
+            searchTextField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    
+}
+
