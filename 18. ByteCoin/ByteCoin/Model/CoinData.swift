c1c89c02e98fbde88078b35c810b4284c94338a5
Mutlu Çalkan <mutlu.calkan@gmail.com>
2022-11-23 11:12:43 +0300

    Created service settings

---
+++ 18. ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Mutlu Çalkan on 23.11.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData : Codable {
+    let rate : Double
+}
---
+++ 18. ByteCoin/ByteCoin/Service/CoinManager.swift
@@ -0,0 +1,48 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "97C3651D-782D-44A6-B5B8-361C7B817D38"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    func getCoinPrice(for currency: String){
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error?.localizedDescription ?? "An error occured while giving a task to the session.")
+                    return
+                }
+                if let data{
+                    let bitcoinPrice = self.parseJSON(data)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double?{
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+        }catch{
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+}
