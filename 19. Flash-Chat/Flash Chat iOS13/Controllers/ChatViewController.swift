6c4bdeb292f76c4bef92a027281e9aa3cbefd9de
Mutlu Çalkan <mutlu.calkan@gmail.com>
2022-11-25 07:25:47 +0300

    Message cell created

--- 19. Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ 19. Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,22 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     
+    var messages : [Messages] = [
+        Messages(sender: "1@2.com", body: "Hey"),
+        Messages(sender: "111@2.com", body: "What's up")
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        // Tableview
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
     }
     
     
@@ -32,6 +43,20 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as? MessageCell else { return UITableViewCell()
+        }
+        cell.label.text = messages[indexPath.row].body
+        return cell
     }
     
 }
---
+++ 19. Flash-Chat/Flash Chat iOS13/Models/Messages.swift
@@ -0,0 +1,14 @@
+//
+//  Messages.swift
+//  Flash Chat iOS13
+//
+//  Created by Mutlu Çalkan on 25.11.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Messages {
+    let sender : String    //E-mail address of sender
+    let body : String      //Message of sender
+}
