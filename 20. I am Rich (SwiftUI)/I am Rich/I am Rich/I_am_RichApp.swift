cfd767733b810bcb859313e842d8b650133f702f
Mutlu Çalkan <mutlu.calkan@gmail.com>
2022-11-28 08:20:21 +0300

    I am Rich project created.

---
+++ 20. I am Rich (SwiftUI)/I am Rich/I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by Mutlu Çalkan on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
