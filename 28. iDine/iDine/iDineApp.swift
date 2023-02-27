d80ac4c07c575a0804895d311226d45275b11c21
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-02-27 17:15:10 +0300

    Menu and Assets created.

---
+++ 28. iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Mutlu Çalkan on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
