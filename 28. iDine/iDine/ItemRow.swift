10ec6fb83fe6bd222e804a4e46e7cad0f30490a1
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-03-01 13:58:10 +0300

    Menu UI improved. Food restrictions added.

--- 28. iDine/iDine/ItemRow.swift
+++ 28. iDine/iDine/ItemRow.swift
@@ -10,14 +10,34 @@ import SwiftUI
 struct ItemRow: View {
     let item: MenuItem
     
+    let restrictColors : [String:Color] = ["D":.purple, "G":.orange, "N":.red, "S":.blue, "V":.green]
+    
     var body: some View {
         HStack{
             HStack{
                 Image(item.thumbnailImage)
-                Text(item.name)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color("priceColor"), lineWidth: 2))
+                VStack(alignment: .leading, spacing: 3){
+                    Text(item.name)
+                        .font(.headline)
+                    HStack{
+                        ForEach(item.restrictions, id: \.self){ restriction in
+                            Text(restriction)
+                                .font(.caption)
+                                .fontWeight(.black)
+                                .padding(5)
+                                .background(restrictColors[restriction, default: .black])
+                                .clipShape(Circle())
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
             }
             Spacer()
-            Text("$\(item.price)").fontWeight(.bold)
+            Text("$\(item.price)")
+                .fontWeight(.black)
+                .foregroundColor(Color("priceColor"))
         }
     }
 }
