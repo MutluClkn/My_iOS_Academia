dca042860dfa69179b636bec5a0a5378c1a364d1
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-03-03 12:22:38 +0300

    Order screen created.

--- 28. iDine/iDine/ItemDetail.swift
+++ 28. iDine/iDine/ItemDetail.swift
@@ -12,6 +12,8 @@ import SwiftUI
 struct ItemDetail: View {
     let item : MenuItem
     
+    @EnvironmentObject var order : Order
+    
     var body: some View {
         VStack{
             ZStack(alignment: .bottomTrailing){
@@ -29,6 +31,11 @@ struct ItemDetail: View {
             Text(item.description)
                 .padding()
             
+            Button("Order"){
+                order.add(item: item)
+            }
+            .buttonStyle(.borderedProminent)
+            
             Spacer()
         }
         .navigationTitle(item.name)
@@ -41,6 +48,7 @@ struct ItemDetail_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack{
             ItemDetail(item: MenuItem.example)
+                .environmentObject(Order())
         }
     }
 }
