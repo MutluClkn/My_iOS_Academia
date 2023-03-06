e24c6c62850c8ed5e7a5abe858e8401c5f5a7fc4
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-03-06 11:56:14 +0300

    Payment method, tip amounts and order confirmation button added on CheckoutView.

--- 28. iDine/iDine/View/CheckoutView.swift
+++ 28. iDine/iDine/View/CheckoutView.swift
@@ -14,18 +14,59 @@ struct CheckoutView: View {
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentType = "Cash"
     
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyID = ""
+    
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15
+    
+    @State private var showPaymentAlert = false
+    
+    var totalPrice : String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
     var body: some View {
-        VStack{
+        Form{
             Section{
                 Picker("How do you want to pay?", selection: $paymentType){
                     ForEach(paymentTypes, id: \.self){
                         Text($0)
                     }
                 }
+                
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyID)
+                }
+            }
+            
+            Section("Add a tip?"){
+                Picker("Percentage:", selection: $tipAmount){
+                    ForEach(tipAmounts, id: \.self){
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Total: \(totalPrice)"){
+                Button("Confirm Order"){
+                    showPaymentAlert.toggle()
+                }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order Confirmed", isPresented: $showPaymentAlert){
+            
+        } message: {
+            Text("Your total was \(totalPrice). Thank you!")
+        }
+        
     }
 }
 
